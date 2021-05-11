2ab9ad148dda13a5de19219aecc480d5c1900a0d
israel <israel.pereira@dcx.ufpb.br>
2021-05-11 10:14:43 -0300

    upgrade do perfil👨‍🎓

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -10,21 +10,21 @@ const Resume = () => {
           <h5 className="my-3 text-2xl font-bold">Education</h5>
           <div className="">
             <h5 className="my-2 text-xl font-bold">
-              Computer Science Engineering
+              system analyst
             </h5>
-            <p className="font-semibold">Academy of Technology (2017-2021)</p>
+            <p className="font-semibold">Federal University of Paraiba (2015-2021)</p>
             <p className="my-3">
-              I am currently pursuing B.tech in Computer Science Engineering
-              from Academy of Technology
+              I am currently pursuing B.tech in Information system
+              from Federal University of Paraiba 
             </p>
           </div>
         </div>
         <div>
           <h5 className="my-3 text-2xl font-bold">Experience</h5>
           <div className="">
-            <h5 className="my-2 text-xl font-bold">Software Developer Jr.</h5>
-            <p className="font-semibold">Tata Consultancy Services</p>
-            <p className="my-3">I don't know why I am doing this job</p>
+            <h5 className="my-2 text-xl font-bold">Software Developer</h5>
+            <p className="font-semibold">Israel Consultancy Services</p>
+            <p className="my-3">freelance work</p>
           </div>
         </div>
       </div>
